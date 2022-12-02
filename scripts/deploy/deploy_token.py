0a970c28d953c4376ae35aead4bc2aaf1c11adf7
0xSacha <90143060+0xSacha@users.noreply.github.com>
2022-12-02 15:52:06 +0100

    add script faucet token

--- scripts/deploy/deploy_token.py
+++ scripts/deploy/deploy_token.py
@@ -3,7 +3,6 @@ from starknet_py.net import AccountClient, KeyPair
 from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.contract import Contract
 from starknet_py.net.udc_deployer.deployer import Deployer
-from utils import str_to_felt, to_uint
 import settings
 import utils
 
@@ -15,379 +14,94 @@ TOKEN_INITIAL_SUPPLY_LO = (10**12)*10**6
 TOKEN_INITIAL_SUPPLY_HI = 0
 
 
-TOKEN_CONTRACT= ['../../lib/openzeppelin/token/erc20/presets/ERC20Mintable.cairo']
-
-ABI = [
-    {
-        "members": [
-            {
-                "name": "low",
-                "offset": 0,
-                "type": "felt"
-            },
-            {
-                "name": "high",
-                "offset": 1,
-                "type": "felt"
-            }
-        ],
-        "name": "Uint256",
-        "size": 2,
-        "type": "struct"
-    },
-    {
-        "data": [
-            {
-                "name": "previousOwner",
-                "type": "felt"
-            },
-            {
-                "name": "newOwner",
-                "type": "felt"
-            }
-        ],
-        "keys": [],
-        "name": "OwnershipTransferred",
-        "type": "event"
-    },
-    {
-        "data": [
-            {
-                "name": "from_",
-                "type": "felt"
-            },
-            {
-                "name": "to",
-                "type": "felt"
-            },
-            {
-                "name": "value",
-                "type": "Uint256"
-            }
-        ],
-        "keys": [],
-        "name": "Transfer",
-        "type": "event"
-    },
-    {
-        "data": [
-            {
-                "name": "owner",
-                "type": "felt"
-            },
-            {
-                "name": "spender",
-                "type": "felt"
-            },
-            {
-                "name": "value",
-                "type": "Uint256"
-            }
-        ],
-        "keys": [],
-        "name": "Approval",
-        "type": "event"
-    },
-    {
-        "inputs": [
-            {
-                "name": "name",
-                "type": "felt"
-            },
-            {
-                "name": "symbol",
-                "type": "felt"
-            },
-            {
-                "name": "decimals",
-                "type": "felt"
-            },
-            {
-                "name": "initial_supply",
-                "type": "Uint256"
-            },
-            {
-                "name": "recipient",
-                "type": "felt"
-            },
-            {
-                "name": "owner",
-                "type": "felt"
-            }
-        ],
-        "name": "constructor",
-        "outputs": [],
-        "type": "constructor"
-    },
-    {
-        "inputs": [],
-        "name": "name",
-        "outputs": [
-            {
-                "name": "name",
-                "type": "felt"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "symbol",
-        "outputs": [
-            {
-                "name": "symbol",
-                "type": "felt"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [
-            {
-                "name": "totalSupply",
-                "type": "Uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "decimals",
-        "outputs": [
-            {
-                "name": "decimals",
-                "type": "felt"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "account",
-                "type": "felt"
-            }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-            {
-                "name": "balance",
-                "type": "Uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "owner",
-                "type": "felt"
-            },
-            {
-                "name": "spender",
-                "type": "felt"
-            }
-        ],
-        "name": "allowance",
-        "outputs": [
-            {
-                "name": "remaining",
-                "type": "Uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [
-            {
-                "name": "owner",
-                "type": "felt"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "recipient",
-                "type": "felt"
-            },
-            {
-                "name": "amount",
-                "type": "Uint256"
-            }
-        ],
-        "name": "transfer",
-        "outputs": [
-            {
-                "name": "success",
-                "type": "felt"
-            }
-        ],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "sender",
-                "type": "felt"
-            },
-            {
-                "name": "recipient",
-                "type": "felt"
-            },
-            {
-                "name": "amount",
-                "type": "Uint256"
-            }
-        ],
-        "name": "transferFrom",
-        "outputs": [
-            {
-                "name": "success",
-                "type": "felt"
-            }
-        ],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "spender",
-                "type": "felt"
-            },
-            {
-                "name": "amount",
-                "type": "Uint256"
-            }
-        ],
-        "name": "approve",
-        "outputs": [
-            {
-                "name": "success",
-                "type": "felt"
-            }
-        ],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "spender",
-                "type": "felt"
-            },
-            {
-                "name": "added_value",
-                "type": "Uint256"
-            }
-        ],
-        "name": "increaseAllowance",
-        "outputs": [
-            {
-                "name": "success",
-                "type": "felt"
-            }
-        ],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "spender",
-                "type": "felt"
-            },
-            {
-                "name": "subtracted_value",
-                "type": "Uint256"
-            }
-        ],
-        "name": "decreaseAllowance",
-        "outputs": [
-            {
-                "name": "success",
-                "type": "felt"
-            }
-        ],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "to",
-                "type": "felt"
-            },
-            {
-                "name": "amount",
-                "type": "Uint256"
-            }
-        ],
-        "name": "mint",
-        "outputs": [],
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "name": "newOwner",
-                "type": "felt"
-            }
-        ],
-        "name": "transferOwnership",
-        "outputs": [],
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "renounceOwnership",
-        "outputs": [],
-        "type": "function"
-    }
-]
 
 async def deploy():
-    goerli2_client = GatewayClient("https://alpha4-2.starknet.io/")
-
-    # deployer = AccountClient(
-    #         client=current_client, 
-    #         address=deployed_accounts[0]["address"],
-    #         key_pair=KeyPair(private_key=773226257887243615725145662263978686691460209496363084148792726531265848363, public_key=3260281675601709103560498194009673788088220875676491641367091347543912979573),
-    #         chain=StarknetChainId.TESTNET,
-    #         supported_tx_version=1
-    #         )
-
+    goerli2_client = GatewayClient(net=settings.NET)
+    keypair = KeyPair(private_key=settings.PRIVATE_KEY, public_key=settings.PUBLIC_KEY)
     admin = AccountClient(
         client=goerli2_client,
-        address="0x06F7F332d385b36342392B0A997eDb2297898B036257f271f082E1DB745d3154",
-        key_pair=KeyPair(private_key=773226257887243615725145662263978686691460209496363084148792726531265848363, public_key=3260281675601709103560498194009673788088220875676491641367091347543912979573),
-        chain=1536727068981429685321,
+        address=settings.ADMIN,
+        key_pair=keypair,
+        chain=settings.CHAIN,
         supported_tx_version=1,
+    )    
+    deployer = Deployer(deployer_address=utils.UD, account_address=admin.address)
+
+    block = await admin.get_block(block_number="latest")
+    print(f'🧱 Current block: {block.block_number}')
+    balance = await admin.get_balance(utils.ETH)
+    print(f'💰 Deployer balance: {balance/(10**18)} ETH')
+
+    print(f'⌛️ Declaring ERC20...')
+    declare_transaction_erc20 = await admin.sign_declare_transaction(
+    compilation_source=utils.ERC20_SOURCE_CODE, max_fee=int(1e16)
     )
 
-# 1536727068981429685321
-    balance = await admin.get_balance("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7")
-    print(f'✨ ERC20 Contract deployed at {balance}')
+    resp = await admin.declare(transaction=declare_transaction_erc20)
+    await admin.wait_for_tx(resp.transaction_hash)
+    erc20_class_hash = resp.class_hash
 
-    # # # If the ABI is known, create the contract directly (this is the preferred way).
-    # eth = Contract(
-    #     2087021424722619777119509474943472645767659996348769578120564519014510906823,
-    #     ABI,
-    #     admin,
-    #     )
+    print(f'✅ Success! Class Hash: {erc20_class_hash} ')
 
-    # # # Calling contract's function doesn't create a new transaction, you get the function's result.
-    # (balance_eth,) = await eth.functions["balanceOf"].call(897)
-    # # # balance = print(f'✨ ERC20 Contract deployed at {balance_eth}')
+
+    print(f'⌛️ Declaring faucet...')
+    declare_transaction_faucet = await admin.sign_declare_transaction(
+    compilation_source=utils.FAUCET_SOURCE_CODE, max_fee=int(1e16)
+    )
+    resp = await admin.declare(transaction=declare_transaction_faucet)
+    await admin.wait_for_tx(resp.transaction_hash)
+    faucet_class_hash = resp.class_hash
+
+    print(f'✅ Success! Class Hash: {faucet_class_hash} ')
+    
+    print(f'⌛️ Declaring erc20...')
+    deploy_erc20_call, erc20 = deployer.create_deployment_call(
+    class_hash=erc20_class_hash,
+    abi=utils.FAUCET_ABI,
+    calldata={
+        "single_value": 10,
+        "tuple": (1, (2, 3)),
+        "arr": [1, 2, 3],
+        "dict": {"value": 12, "nested_struct": {"value": 99}},
+    },
+
+    # Once call is prepared, it can be executed with an account (preferred way)
+    resp = await account_client.execute(deploy_call, max_fee=int(1e16))
+
+    # Or signed and send with an account
+    invoke_tx = await account_client.sign_invoke_transaction(
+        deploy_call, max_fee=int(1e16)
+    )
+    resp = await account_client.send_transaction(invoke_tx)
+
+    # Wait for transaction
+    await account_client.wait_for_tx(resp.transaction_hash)
+
+
+    deploy_erc20_call, erc20 = deployer.create_deployment_call(
+    class_hash=erc20_class_hash,
+    abi=utils.FAUCET_ABI,
+    calldata={
+        "single_value": 10,
+        "tuple": (1, (2, 3)),
+        "arr": [1, 2, 3],
+        "dict": {"value": 12, "nested_struct": {"value": 99}},
+    },
+
+    # Once call is prepared, it can be executed with an account (preferred way)
+    resp = await account_client.execute(deploy_call, max_fee=int(1e16))
+
+    # Or signed and send with an account
+    invoke_tx = await account_client.sign_invoke_transaction(
+        deploy_call, max_fee=int(1e16)
+    )
+    resp = await account_client.send_transaction(invoke_tx)
+
+    # Wait for transaction
+    await account_client.wait_for_tx(resp.transaction_hash)
+
+)
+
 
 
 loop = asyncio.get_event_loop()
--- scripts/deploy/utils.py
+++ scripts/deploy/utils.py
@@ -21,14 +21,21 @@ def long_str_to_print_array(text):
 def decimal_to_hex(decimal: int):
     return hex(decimal)
 
-ETH = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
-
 
 ## VARIOUS
-
 SALT = 0
 
 
-## ABI
-ERC20_ABI="./BI/erc20_abi.json"
+## CONTRACTS SOURCE CODE
+ERC20_SOURCE_CODE = "../../lib/openzeppelin/token/erc20/presets/ERC20Mintable.cairo"
+FAUCET_SOURCE_CODE = "../../lib/utils/faucet.cairo"
 
+## CONTRACTS ABI
+ERC20_ABI = "../../build/erc20_abi.json"
+FAUCET_ABI ="../../build/faucet_abi.json"
+
+
+
+## CONTRACTS ADDRESSES
+ETH = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+UD = "0x041a78e741e5af2fec34b695679bc6891742439f7afb8484ecd7766661ad02bf"
